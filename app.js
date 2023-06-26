022153af7495971f2562ab105946d434c4485bab
Trần Ngọc Anh <anhtn1@vincss.local>
2023-06-26 19:32:36 +0700

    remove comment in app.js

--- app.js
+++ app.js
@@ -52,41 +52,15 @@ async function listenLatestBlock() {
           //   console.log(JSON.stringify(transaction));
           //   actions.push(action);
           // }
+
           if (isContractFollow(transaction)) {
             //send to python process
             console.log(JSON.stringify(transaction));
             pushData(transaction);
           }
+
           // DEBUG
           // console.log(JSON.stringify(transaction));
-          // const actionObject = transaction.actions[0];
-          // const action_type = Object.keys(actionObject)[0];
-          // const detail = actionObject[action_type];
-          // let temp = {
-          //   hash: transaction.hash,
-          //   signer_id: transaction.signer_id,
-          //   receiver_id: transaction.receiver_id,
-          //   action_type: action_type
-          // }
-          // if (action_type === 'FunctionCall') {
-          //   try {
-          //     temp.action_detail = {
-          //       method_name: detail.method_name,
-          //       args: JSON.parse(atob(detail.args)),
-          //       deposit: detail.deposit,
-          //       gas: detail.gas
-          //     }
-          //   } catch (error) {
-          //     temp.action_detail = detail;
-          //   }
-          // } else {
-          //   temp.action_detail = detail;
-          // }
-          // if (isContractFollow(temp)) {
-          //   console.log(temp);
-          //   console.log(JSON.stringify(temp.action_detail.args,null,2));
-          //   console.log('==============================')
-          // }
         }
       }
     }
