9b7cfd16c69580ef3e260117a0867279cf34c1a7
Trần Ngọc Anh <anhtn1@vincss.local>
2023-06-26 19:27:18 +0700

    add more config

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     NEAR_RPC_API: 'https://rpc.testnet.near.org',
-    follow_address: ['dev-1684410668019-65962341802455']
+    follow_address: ['dev-1684410668019-65962341802455'],
+    push_url: 'http://localhost:5000/push'
 }
